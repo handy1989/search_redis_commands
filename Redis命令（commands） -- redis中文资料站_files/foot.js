f65d65015afab99a762a0f4b31715f439abfc510
LittleWhite <menghan1218@gmail.com>
2015-04-01 17:53:45 +0800

    添加Redis命令网页文档

---
+++ Redis命令（commands） -- redis中文资料站_files/foot.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+var footHTML = "";
+footHTML += '<footer>';
+footHTML += '     <div class="sponsor">';
+footHTML += '        本站资源翻译自<a href="http://redis.io" target="_blank">redis.io</a>，由<a href="/aboutus.html">redis.cn翻译团队</a>，更新日志请点击<a href="/update.html">这里</a>查看，翻译原文版权归redis.io官方所有，翻译不正确的地方欢迎大家指出。<br/>';
+footHTML += '        联系Email:<a href="mailto:admin@redis.cn">admin@redis.cn</a>，redis交流群：<a href="#">46859267</a> &nbsp; ';
+footHTML += "<scr";
+footHTML += "ipt src='http://s22.cnzz.com/stat.php?id=3593514&web_id=3593514' langu";
+footHTML += "age='JavaScript'></scr";
+footHTML += "ipt><br/>";
+footHTML += '<span class="jiathis_txt">分享到：</span>';
+footHTML += '<a class="jiathis_button_tools_1"></a>&nbsp;&nbsp;';
+footHTML += '<a class="jiathis_button_tools_2"></a>&nbsp;&nbsp;';
+footHTML += '<a class="jiathis_button_tools_3"></a>&nbsp;&nbsp;';
+footHTML += '<a class="jiathis_button_tools_4"></a>&nbsp;&nbsp;';
+footHTML += '<a href="http://www.jiathis.com/share?uid=1875726" class="jiathis jiathis_txt jiathis_separator jtico jtico_jiathis" target="_blank">更多</a>&nbsp;&nbsp;';
+footHTML += '<a class="jiathis_counter_style"></a>';
+footHTML += '      </div>';
+footHTML += '    </footer>';
+	
+// 分享插件	
+var jiathis_config = {data_track_clickback:'true'};
+footHTML += '<scr'+'ipt type="text/javascript" src="http://v3.jiathis.com/code/jia.js?uid=1875726" charset="utf-8"></scr'+'ipt>';
+
+document.write(footHTML);
